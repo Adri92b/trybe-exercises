6b79f3211c8707153608a85fa748ba622a03d8fe
Adriana Barreto <adri92barreto123@gmail.com>
2021-05-19 18:51:11 -0300

    Soluciona segunda questao

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -1,8 +1,15 @@
-let a= 9;
-let b= 2;
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+ 
+let x= 4;
+let y= 8; 
+
+if(x>y) {
+    console.log ("x é maior que y")
+}else {
+    console.log ("y é menor que x")
+}
+
+
+
+
