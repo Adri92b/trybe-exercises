5ecd5898a7d8d8ae5bf0725ac8d80488e2725832
Adriana Barreto <adri92barreto123@gmail.com>
2021-05-19 19:13:03 -0300

    Soluciona terceira questao

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -1,5 +1,16 @@
+//Questão 1
+ 
+let a= 1;
+let b= 2;
 
-// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+
+// // Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
  
 let x= 4;
 let y= 8; 
@@ -10,6 +21,19 @@ if(x>y) {
     console.log ("y é menor que x")
 }
 
+// Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
 
+let a= 1;
+let b= 2;
+let c= 3;
 
+if(a>b && a>c) {
+  console.log ("a é maior do que b e c")
+}
+else if(b>a && b>c) {
+  console.log ("b é maior do que a e c")
+}
+else {
+  console.log ("c é maior do que a e b")
+}
 
