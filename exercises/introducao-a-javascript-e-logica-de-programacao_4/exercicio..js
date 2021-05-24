a20bef6469ab5b4bd21f8918920e86a69d3fcd99
Adriana Barreto <adri92barreto123@gmail.com>
2021-05-24 18:42:35 -0300

    Resolve segunda questao

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio..js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio..js
@@ -4,10 +4,9 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
-  console.log('Bem-vinda,' + ' ' + info.personagem);
+//   console.log('Bem-vinda,' + ' ' + info.personagem);
 
 
+info.recorrente = "Sim";
 
-
-//   Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-// Valor esperado no console: Bem-vinda, Margarida
+console.log(info);
