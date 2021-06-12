898113020a21f2e0b8f82c3abdbed741928c0901
Adriana Barreto <adri92barreto123@gmail.com>
2021-06-12 13:07:27 -0300

    Adiciona alguns requesitos

--- exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -1,5 +1,6 @@
-function creatEstados() {
-  let estadosBrasil = ['AC- Acre', 'AL- Alagoas', 'AP- Amapá',
+function createEstados() {
+  let estadosSelect = document.getElementById('estado');
+  let optionsEstado = ['AC- Acre', 'AL- Alagoas', 'AP- Amapá',
   'AM- Amazonas', 'BA- Bahia', 'CE- Ceará', 'DF- Distrito Federal',
   'ES- Espírito Santo', 'GO- Goiás', 'MA- Maranhão', 'MT- Mato Grosso',
   'MS- Mato Grosso do Sul', 'MG- Minas Gerais', 'PA- Pará', 'PB- Paraíba',
@@ -7,12 +8,10 @@ function creatEstados() {
   'RN- Rio Grande do Norte', 'RS- Rio Grande do Sul', 'RO- Rondônia',
   'RR- Roraima', 'SC- Santa Catarina', 'SP- São Paulo', 'SE- Sergipe',
   'TO- Tocantins']
-  for (creatEstados in estadosBrasil) {
-      let elemento = document.createElement('option');
-      let pai = document.querySelector('select');
-      pai.appendChild(elemento);
-      elemento.getAttribute('required');
-      elemento.getAttribute('class', 'estadoSelecao');
+  for (let index = 0; index < optionsEstado.length; index += 1) {
+      let criarOptions = document.createElement('option');
+      estadosSelect.appendChild(criarOptions).innerText = optionsEstado[index];
+      estadosSelect.appendChild(criarOptions).value = optionsEstado[index];
   }
 }
-creatEstados()
+createEstados()
